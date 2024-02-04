4a12152edb21d4df23b773afd9ddbb2346b11b35
ankush <ankushsingh3314260@gmail.com>
2024-02-04 20:20:08 +0530

    changed

--- src/animation.jsx
+++ src/animation.jsx
@@ -35,7 +35,7 @@ const Animation =()=>{
                 <div  id ="intro-slider"className="h-screen max-sm:absolute  max-sm:w-screen p-10 bg-white  w-full absolute top-0 left-0 z-10 flex flex-col">
                     <div className="max-sm:relative max-sm:top-20">
                     <h1  id="title1" className="text-9xl  max-sm:relative  max-sm:text-6xl">Welcome</h1>
-                    <h1 id="title2" className="text-9xl max-sm:text-6xl ">Thetext</h1>
+                    <h1 id="title2" className="text-9xl max-sm:text-6xl ">Ankush's</h1>
                     <h1 id="title3" className="text-9xl max-sm:text-6xl  " >Portfolio</h1>
                     </div>
                 </div>
