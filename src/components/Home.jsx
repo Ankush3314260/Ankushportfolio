298df665e87c08da5ecd0bc8979bb9593d152ae8
ankush <ankushsingh3314260@gmail.com>
2024-02-14 13:11:47 +0530

    removed type js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React,{useEffect} from "react"
 import { ReactTyped } from "react-typed";
 
 function Home(){
-         document.querySelector("body").style.overflowX="hidden"
+      
       
      const sendmail =()=>{
           
@@ -29,29 +29,10 @@ function Home(){
                   <svg className=' absolute ml-1 max-sm:w-16 top-0 max-sm:top-1/2 -z-10 max-sm:right-10  right-36 rotate-6 opacity-50  ' xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1052 1052"><path fill="#f0db4f" d="M0 0h1052v1052H0z"/><path d="M965.9 801.1c-7.7-48-39-88.3-131.7-125.9-32.2-14.8-68.1-25.399-78.8-49.8-3.8-14.2-4.3-22.2-1.9-30.8 6.9-27.9 40.2-36.6 66.6-28.6 17 5.7 33.1 18.801 42.8 39.7 45.4-29.399 45.3-29.2 77-49.399-11.6-18-17.8-26.301-25.4-34-27.3-30.5-64.5-46.2-124-45-10.3 1.3-20.699 2.699-31 4-29.699 7.5-58 23.1-74.6 44-49.8 56.5-35.6 155.399 25 196.1 59.7 44.8 147.4 55 158.6 96.9 10.9 51.3-37.699 67.899-86 62-35.6-7.4-55.399-25.5-76.8-58.4-39.399 22.8-39.399 22.8-79.899 46.1 9.6 21 19.699 30.5 35.8 48.7 76.2 77.3 266.899 73.5 301.1-43.5 1.399-4.001 10.6-30.801 3.199-72.101zm-394-317.6h-98.4c0 85-.399 169.4-.399 254.4 0 54.1 2.8 103.7-6 118.9-14.4 29.899-51.7 26.2-68.7 20.399-17.3-8.5-26.1-20.6-36.3-37.699-2.8-4.9-4.9-8.7-5.601-9-26.699 16.3-53.3 32.699-80 49 13.301 27.3 32.9 51 58 66.399 37.5 22.5 87.9 29.4 140.601 17.3 34.3-10 63.899-30.699 79.399-62.199 22.4-41.3 17.6-91.3 17.4-146.6.5-90.2 0-180.4 0-270.9z" fill="#323330"/></svg>
             
                     <div className="  w-11/12    max-sm:m-6 max-sm:w-11/12  sm:m-auto max-sm:relative   ">
-                           <h1 className="text-6xl max-sm:text-5xl  font-bold w-11/12 text-center  im ">I'M</h1>
-                           <div  id="name" className="  text-8xl max-sm:text-6xl  max-sm:justify-center max-sm:relative  font-bold w-11/12 text-center  flex flex-wrap justify-center "><p className="downside "> ANKUSH </p> <p className="leftside"> KUMAR</p> </div>
-                        <div  className="w-11/12 text-center justify-center text-4xl flex-wrap max-sm:text-2xl   flex  ">
-    <ReactTyped strings={["And Skilled With"]} typeSpeed={40} />
-    <br />
-
-    <ReactTyped
-      strings={[
-        "Front-End Development",
-        "UI/UX Designs",
-        "Java Script ",
-        "Tailwind Css",
-        "Video Editing"
-      ]}
-      typeSpeed={40}
-      backSpeed={50}
-     
-      loop
-    >
-      <input className="text-blue-300 outline-none max-sm:relative max-sm:left-10 " type="text" />
-    </ReactTyped>
-  </div>
-                           <div  className="max-sm:text-md relative m-2 text-xl max-sm:top-3 top-2 ">Profile  Summary:
+                           <h1 className="text-6xl max-sm:text-5xl  font-bold w-11/12 text-center  im  ">I'M</h1>
+                           <div  id="name" className="  text-8xl max-sm:text-6xl  max-sm:justify-center  max-sm:relative  font-bold w-11/12 text-center  flex flex-wrap justify-center "><p className="downside "> ANKUSH </p> <p className="leftside"> KUMAR</p> </div>
+                       
+                           <div  className="max-sm:text-md relative m-2 text-xl max-sm:top-3 top-2  ">Profile  Summary:
                             <p>Passionate about coding, and Software Development. Thrive in a dynamic development environment. Bringing fresh ideas and a strong work ethic to a forward-thinking organization.
                                 Let's connect! </p>
                            </div>
